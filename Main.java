f216cf9ba8ee038ef8034a83b128bae0a9771d7b
durvesh <thes86088@gmail.com>
2026-03-03 19:06:42 +0530

    add test cases for all supported characters
    no errors, but many bugs

--- Main.java
+++ Main.java
@@ -645,9 +645,20 @@ public class Main
 	    
 	    Canvas canvas = new Canvas( size ) ;
 	    
-	    char given = 'A' ;
-	    canvas.drawChar( given ) ;
+	    char[] supportedCharSet =  {
+	        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+            'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+	    };
 	    
-	    canvas.printCanvas() ;
+	    for( char given : supportedCharSet )
+	    {
+	        canvas.drawChar( given ) ;
+	    
+	        canvas.printCanvas() ;
+	        System.out.println(); System.out.println();
+	    }
 	}
 }
