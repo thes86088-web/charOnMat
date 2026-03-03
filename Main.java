4a8d6ac2d4b9a79bf16ebc9f1dc444d37f8927c6
durvesh <thes86088@gmail.com>
2026-03-03 12:58:20 +0530

    add a skeleton for main method

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int size = 3 ; //some integer>2
+	    int height = (3*size) + 1;
+	    int width = (2*size) + 1 ;
+	    int canvas[][] = new int[height][width] ;
+		
+	}
+}
